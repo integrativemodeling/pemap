9ec6a2ec77da599074fd8ba29dc708998e217e6d
iecheverria <iecheverria@gmail.com>
2019-11-06 12:11:58 -0800

    benchmark datasets, 40%

---
+++ modeling_histones/analysis_40_1/clustering/run_clustering.sh
@@ -0,0 +1,15 @@
+module load python/pyrmsd
+
+export top_dir=/home/ignacia/Research/pE-MAP/mod_hist_40/
+export analys_dir=$top_dir/analys
+export mod_dir=$analys_dir/GSMs_cl1/
+export name=HIS_hist_40
+
+cp $top_dir/density.txt $mod_dir
+cp $mod_dir/sample_A.txt $mod_dir/Scores_A.txt
+cp $mod_dir/sample_B.txt $mod_dir/Scores_B.txt
+
+ls -lta $analys_dir/GSMs_cl1/sample_A | awk '{print $9}' | grep 'rmf3' > $analys_dir/selected_models_A_cluster1_random.dat
+ls -lta $analys_dir/GSMs_cl1/sample_B | awk '{print $9}' | grep 'rmf3' > $analys_dir/selected_models_B_cluster1_random.dat
+
+nohup ~/SOFTW/imp_november/setup_environment.sh python ~/SOFTW/imp-sampcon/pyext/src/Master_Sampling_Exhaustiveness_Analysis.py --sysname $name --path $mod_dir --mode cuda --align --density density.txt --gridsize 5.2 --gnuplot --scoreA Scores_A.txt --scoreB Scores_B.txt  --rmfs_A $analys_dir/selected_models_A_cluster1_random.dat --rmfs_B $analys_dir/selected_models_B_cluster1_random.dat --subsample 20000 > clustering.log &
---
+++ modeling_histones/analysis_40_3/clustering/run_clustering.sh
@@ -0,0 +1,16 @@
+module load python/pyrmsd
+
+export cl=0
+export top_dir=/home/ignacia/Research/pE-MAP/mod_hist_40_3/
+export analys_dir=$top_dir/analys
+export mod_dir=$analys_dir/GSMs_cl$cl/
+export name=HIS_hist
+
+cp /home/ignacia/Research/pE-MAP/mod_hist_80/density.txt $mod_dir
+cp $mod_dir/sample_A.txt $mod_dir/Scores_A.txt
+cp $mod_dir/sample_B.txt $mod_dir/Scores_B.txt
+
+ls -lta $analys_dir/GSMs_cl${cl}/sample_A | awk '{print $9}' | grep 'rmf3' > $analys_dir/selected_models_A_cluster${cl}_random.dat
+ls -lta $analys_dir/GSMs_cl${cl}/sample_B | awk '{print $9}' | grep 'rmf3' > $analys_dir/selected_models_B_cluster${cl}_random.dat
+
+nohup ~/SOFTW/imp_november/setup_environment.sh python ~/SOFTW/imp-sampcon/pyext/src/Master_Sampling_Exhaustiveness_Analysis.py --sysname $name --path $mod_dir --mode cuda --align --density density.txt --gridsize 3.0 --gnuplot --scoreA Scores_A.txt --scoreB Scores_B.txt  --rmfs_A $analys_dir/selected_models_A_cluster${cl}_random.dat --rmfs_B $analys_dir/selected_models_B_cluster${cl}_random.dat --subsample 20000 > clustering.log &
